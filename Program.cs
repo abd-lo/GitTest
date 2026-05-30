080748cce479eb69bab7cfac0f599bc2a6415f1a
Abd <abd.loubani1@gmail.com>
2026-05-30 18:13:54 +0300

    intializing commite

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+            Console.WriteLine(	"b brnachdd test");
+            Console.WriteLine(	"2 brnachdd test");
+            Console.WriteLine(	"new branch");
+            Console.WriteLine(	"new branch1");
+
+        }
+	}
+}
